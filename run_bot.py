b38529ebb3be627df65db885027977ba3e5fd14c
tacocat <jackjm22@outlook.com>
2015-12-08 13:34:48 -0500

    Fixed small error

--- run_bot.py
+++ run_bot.py
@@ -2,22 +2,26 @@ import socket
 
 # Class the connects to server and retrieves/sends
 class connection2:
-
-	def __init__(self, server, channel, botnick, port):
+	
+    def __init__(self, server, channel, botnick, port):
 		ircsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		ircsocket.connect((server, port)) # Here we connect to the server using the port 6667
-		ircsocket.send("USER "+ botnick +" "+ botnick +" "+ botnick +" :This bot is a result of a tutoral covered on http://shellium.org/wiki.\n") # user authentication
-		ircsocket.send("NICK "+ botnick +"\n") # here we actually assign the nick to the bot
+		ircsocket.connect((server, port))
+		ircsocket.send("USER "+ botnick +" "+ botnick +" "+ botnick + botnick)
+		ircsocket.send("NICK "+ botnick +"\n") 
 		ircsocket.send("JOIN "+ channel +"\n")
 
 		while 1:
-			ircmsg = ircsocket.recv(2048) 
-			ircmsg = ircmsg.strip('\n\r') 
-			print(ircmsg) 
+			ircmsg = ircsocket.recv(2048)
+			ircmsg = ircmsg.strip('\n\r')
+			print(ircmsg)
+
+			if ircmsg.find("$taco") != -1: # if the server pings us then we've got to respond!
+				print "taco"
+
 
 # Simple class containing different commands
-class commands:
-	
+#class commands:
+    	
 
 #Basic configuration infomration
 server = "irc.rizon.net"
